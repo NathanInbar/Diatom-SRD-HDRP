7e28549d55ae99f06ef2999e2c46f51388b60521
nathan inbar <nathaninbar@gmail.com>
2025-10-28 23:45:08 -0700

    applied random rotation to diatoms

---
+++ Assets/Scripts/RotateAllChildrenRandom.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Transform))]
+public class RandomChildRotationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Transform parentTransform = (Transform)target;
+
+        if (GUILayout.Button("Apply Random Rotation to Children"))
+        {
+            ApplyRandomRotation(parentTransform);
+        }
+    }
+
+    private void ApplyRandomRotation(Transform parent)
+    {
+        Undo.RecordObject(parent, "Randomize Child Rotations");
+
+        foreach (Transform child in parent)
+        {
+            Undo.RecordObject(child, "Randomize Rotation");
+            child.localRotation = Random.rotation;
+        }
+
+        EditorUtility.SetDirty(parent);
+    }
+}
